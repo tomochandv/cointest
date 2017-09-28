b0612a436cce822546d530fb447f9f2d6a2c222e
tomochan shim <tomochanshim@tomochanui-iMac.local>
2017-09-28 17:28:04 +0900

    ddddd

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const path = require('path')
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
+const rp = require('request-promise')
 
 const bitcoin = require('bitcoinjs-lib')
 const blockchain = require('blockchain.info')
@@ -21,19 +22,64 @@ io.sockets.on('connection', function (socket) {
     if(type == 'btc'){
       blockexplorer.getAddress(addr, null).then(function(hisData){
         console.log(hisData)
-        io.emit('address', hisData);
+        io.emit('btc', hisData)
+      })
+    }
+    else{
+      const options = {
+        uri : 'https://api.etherscan.io/api?module=account',
+        qs : {
+          apikey : 'PFC28P6RJ5THARD1T6FFVKEBKYEIS9FF1H',
+          module : 'account',
+          action : 'balance',
+          tag : 'latest',
+          address : addr
+        },
+        headers: {
+          'User-Agent': 'Request-Promise'
+        },
+        json: true
+      }
+
+      const optionsTran = {
+        uri : 'https://api.etherscan.io/api?module=account',
+        qs : {
+          apikey : 'PFC28P6RJ5THARD1T6FFVKEBKYEIS9FF1H',
+          module : 'account',
+          action : 'txlist',
+          startblock : 0,
+          endblock : 99999999999,
+          sort : 'desc',
+          address : addr
+        },
+        headers: {
+          'User-Agent': 'Request-Promise'
+        },
+        json: true
+      }
+      let result = {
+        balance : 0,
+        tran : {}
+      }
+      rp(options)
+      .then(function (repos) {
+        result.balance = repos.result
+        rp(optionsTran)
+        .then(function (repos) {
+          result.tran = repos.result
+          io.emit('eth', result)
+        })
+        .catch(function (err) {
+          console.log(err)
+        })
+      })
+      .catch(function (err) {
+        console.log(err)
       })
     }
   })
 })
 
 http.listen(8080, function(){
-  console.log('listening on *:80');
+  console.log('listening on *:8080');
 })
-
-
-function getEth(value){
-    console.log(value)
-    const mode = value / wei
-    return mode.toFixed(3)
-}
